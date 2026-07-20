d1992f1f44602fc489123cfba93d8509b82990be
IreneSophia <10planki@gmail.com>
2026-07-20 20:12:57 +0930

    Incorporate the possibility of using a specific Timestamp for each data collection point instead of the theoretical fps

--- R/featHead.R
+++ R/featHead.R
@@ -4,7 +4,7 @@
 #' for a given column. 
 #'
 #' @param df Dataframe. The dataset containing the variables to be processed. 
-#'   Must explicitly feature columns `Dyad`, `Identifier`, `Frame` and the column `colname`. 
+#'   Must explicitly feature columns `Dyad`, `Identifier`, either `Frame` or `Timestamp` and the column `colname`. 
 #' @param rs.path Character. Path to destination directory for saved files. If empty (is.null(rs.path) == TRUE), then nothing is saved.
 #' @param colname Character The exact name in \code{df} from which to extract the data. 
 #' @param fps Numeric. Frame processing rate frequency profile (frames per second) of the dataset.
@@ -23,12 +23,6 @@
 compWT = function(df, rs.path, colname, fps, order = 6, suffix = "", 
                   verbose = T, recompute = F, return = T) {
   
-  # configuration for wavelet transformation
-  dt = 1 / fps
-  pad = TRUE # padding with zeros to length of power of 2, faster and less cone of influence problems
-  Dj = 1 / 12 # spacing / resolution between scales, 12 sub-octaves for fine frequency resolution
-  S0 = 2 * dt # smallest scale, set here to Nyquist limit
-  
   # extract the individual Identifiers
   ls.IDs = unique(df$Identifier)
   nos    = length(ls.IDs)
@@ -36,30 +30,55 @@ compWT = function(df, rs.path, colname, fps, order = 6, suffix = "",
   # output structure
   ls.out = list()
   
+  # extract the timeseries from either Timestamp or Frame + fps
+  if ("Timestamp" %in% colnames(df)) {
+    df = df |> group_by(Dyad, Identifier) |> 
+      arrange(Timestamp) |>
+      mutate(start = min(Timestamp), 
+             Timecourse = Timestamp - start,
+             Duration = Timecourse - lag(Timecourse)) |>
+      select(-start)
+  } else {
+    df = df |> group_by(Dyad, Identifier) |> 
+      arrange(Frame) |>
+      mutate(Timecourse = Frame/fps,
+             Duration   = 1/fps)
+  }
+  
   # loop through the Identifiers
   for (i in 1:nos) {
     
     # focus on this Identifier
     df.sel = df |> 
-      filter(Identifier == ls.IDs[i]) |>
-      arrange(Frame)
+      filter(Identifier == ls.IDs[i])
+    
+    # get the timestep
+    dt = mean(df.sel$Duration, na.rm = T)
     
     # extract data into a vector
     data = df.sel |> pull(colname)
     
-    # extract time into a vector
-    time = df.sel |> pull(Frame)/fps
+    # extract the time into a vector
+    time = df.sel |> pull(Timecourse)
     
-    # configure number of frequency scales (minus 1)
-    J1 = round(log2(((length(time) * 0.17) * 2 * dt) / S0) / Dj)
+    # configuration for wavelet transformation
+    S0 = 2 * dt # smallest scale, set here to Nyquist limit
+    Dj = 1/12
+    J1 = round(log2(((length(time) * 0.17) * 2 * dt) / S0) / Dj) # configure number of frequency scales (minus 1)
     
     # compute the wavelet transformation
+    # comparable to MATLAB's wavelet() by Torrence & Compo
     wt = biwavelet::wt(cbind(time, data), 
-                       pad = pad, dj = Dj, s0 = S0, J1 = J1, 
-                       mother = "morlet", param = order)
+                       pad = T, # padding with zeros to length of power of 2, faster and less cone of influence problems
+                       dt  = dt,
+                       dj  = Dj, # spacing / resolution between scales, 12 sub-octaves for fine frequency resolution
+                       s0  = S0, 
+                       J1  = J1, 
+                       mother = "morlet", 
+                       param = order)
     
     # store results
-    ls.out[[i]] = list(wt)
+    ls.out[i] = list(wt)
     names(ls.out)[i] = paste0(df.sel$Dyad[1], "_", ls.IDs[i])
     
   }
