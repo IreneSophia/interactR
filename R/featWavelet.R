cd6ea85596e21359ac0e7fc2cd3cd415a83b40f6
IreneSophia <10planki@gmail.com>
2026-07-24 12:35:19 +0930

    Renamed the function that computes WTC to extract WTC, otherwise might be confused with comparison. Added some plotting functions

--- R/featWavelet.R
+++ R/featWavelet.R
@@ -2,14 +2,14 @@
 #'
 #' Uses the \code{\link{biwavelet::wtc}} function to compute the Wavelet Coherence
 #' and Phase for a given column for a dyad. Optionally creates pseudo coherence based 
-#' on Dyad shuffling. Resulting list can be transformed to dataframe using `[!MISSING]`.
-#' Observed and pseudo coherence can be compared using `[!MISSING]`
+#' on Dyad shuffling. Resulting list can be transformed to dataframe using [convertWTC()].
+#' Observed and pseudo coherence can be compared using [featWTC()].
 #'
 #' @param df Dataframe. The dataset containing the variables to be processed. 
 #'   Must explicitly feature columns `Dyad`, `Identifier`, `Time`, either `Frame` or `Timestamp`, and the column `colname`. 
 #'   For each Dyad, there must be exactly two Identifiers in the data. 
 #' @param rs.path Character. Path to destination directory for saved files. If empty (is.null(rs.path) == TRUE), then nothing is saved.
-#' @param colname Character The exact name in \code{df} from which to extract the data. 
+#' @param colname Character. The exact name in \code{df} from which to extract the data. 
 #' @param fps Numeric. Frame processing rate frequency profile (frames per second) of the dataset.
 #' @param order Numeric. Order for the wavelet transformation. Default is `8` based on Issartel et al. (2006).
 #' @param suffix Character. Suffix to be added to the files saved to disk. Default is `""`.
@@ -23,19 +23,19 @@
 #' @param recompute Logical. Whether existing data on disk should be recomputed and overwritten. Default is `FALSE`.
 #' @param return Logical. Whether the processed dataframe should be returned by the function. Default is `TRUE`.
 #'
-#' @return If `return = TRUE`, returns list with the results. If provided, the full list are saved as rds to `rs.path`.
+#' @return If `return = TRUE`, returns list with the results. If provided, the full list is saved as rds to `rs.path`.
 #' 
 #' @author Irene Sophia Plank (\email{10planki@@gmail.com})
+#' @seealso \code{\link{convertWTC}} \code{\link{featWTC}}
 #' @references Issartel et al. (2006): A Practical Guide to Time—Frequency Analysis 
 #'    in the Study of Human Motor Behavior: The Contribution of Wavelet Transform, 
 #'    Journal of Motor Behavior, 38(2), 139-159.
 #' @import dplyr
 #' @export
 #' 
-
-compWTC = function(df, rs.path, colname, fps, order = 8, suffix = "", 
-                   nsim = 400, pseudoDyad = F, nDyads = NULL, seed = "random",
-                   verbose = T, recompute = F, return = T) {
+extractWTC = function(df, rs.path, colname, fps, order = 8, suffix = "", 
+                      nsim = 400, pseudoDyad = F, nDyads = NULL, seed = "random",
+                      verbose = T, recompute = F, return = T) {
   
   # get a random seed
   if (!is.numeric(seed)) {
@@ -65,7 +65,7 @@ compWTC = function(df, rs.path, colname, fps, order = 8, suffix = "",
     }
   } else {
     
-    if (verbose) cat(format(Sys.time(), "%x %X %Z"), ": Exctracting WTC features from ", colname, "\n")
+    if (verbose) cat(format(Sys.time(), "%x %X %Z"), ": Extracting WTC features from ", colname, "\n")
     
     # check whether all columns in dataframe
     cols = c("Dyad", "Identifier", "Time", "Frame", colname)
@@ -170,30 +170,118 @@ compWTC = function(df, rs.path, colname, fps, order = 8, suffix = "",
   
 }
 
+#' Convert list of wavelet coherence objects into a dataframe
+#'
+#' Takes the list created by [extractWTC()] and transforms it into a dataframe, 
+#' allowing easy aggregation and adding plotting options.
+#'
+#' @param ls List. This list must contain objects created by \code{\link{biwavelet::wtc}}.
+#'   Names of the list entries should follow this structure: `[Dyad]_[Time]`.
+#' @param rs.path Character. Path to destination directory for saved files. If empty (is.null(rs.path) == TRUE), then nothing is saved.
+#' @param featname Character. The name of the feature of which WTC was computed.
+#' @param suffix Character. Suffix to be added to the files saved to disk. Default is `""`.
+#' @param verbose Logical. Whether progress and output are printed to the console. Default is `TRUE`.
+#' @param recompute Logical. Whether existing data on disk should be recomputed and overwritten. Default is `FALSE`.
+#' @param return Logical. Whether the processed dataframe should be returned by the function. Default is `TRUE`.
+#'
+#' @return If `return = TRUE`, returns dataframe. If provided, the full dataframe is saved as rds to `rs.path`.
+#' 
+#' @seealso \code{\link{featWTC}} \code{\link{extractWTC}} \code{\link{plotPhaseRose}} \code{\link{plotWTCLine}}
+#' @author Irene Sophia Plank (\email{10planki@@gmail.com})
+#' @import dplyr
+#' @export
+#' 
+convertWTC = function(ls, rs.path, featname, suffix = "", 
+                   verbose = T, recompute = F, return = T) {
 
-# # convert wavelet coherence list into dataframe
-# df.out = rbind(
-#   df.out, 
-#   data.frame(wtc[["rsq"]]) |>
-#     mutate(Period = wtc[["period"]]) |>
-#     tidyr::pivot_longer(cols = starts_with("X"), values_to = "Rsq") |>
-#     merge(data.frame(wtc[["phase"]]) |>
-#             mutate(Period = wtc[["period"]]) |>
-#             tidyr::pivot_longer(cols = starts_with("X"), values_to = "Phase")) |>
-#     merge(data.frame(wtc[["signif"]]) |>
-#             mutate(Period = wtc[["period"]]) |>
-#             tidyr::pivot_longer(cols = starts_with("X"), values_to = "PermProb")) |>
-#     arrange(name) |>
-#     mutate(
-#       Timecourse = rep(wtc[["t"]], each = length(wtc[["period"]])),
-#       COI   = rep(wtc[["coi"]], each = length(wtc[["period"]])),
-#       Frame = as.numeric(gsub("X", "", name)),
-#       Frequency = 1 / Period,
-#       WithinCOI = Period < COI 
-#     ) |> select(-name) |>
-#     mutate(
-#       Dyad = df.dyad$Dyad[i],
-#       Time = t,
-#       pseudoDyad = pseudoDyad
-#     )
-# )
+  # check rs.path
+  if (is.null(rs.path)) {
+    # create empty filename because nothing will be saved
+    flnm = ''
+  } else {
+    # create filename 
+    flnm  = file.path(rs.path, sprintf("dataWTC_%s-df%s.rds", 
+                                       featname, suffix))
+  }
+  
+  # if no recompute and the file exists, it is simply loaded
+  if (!recompute & file.exists(flnm)) {
+    if (return) {
+      if (verbose) cat(format(Sys.time(), "%x %X %Z"), ": Loading WTC feature dataframe\n")
+      df.out = readRDS(flnm)
+    }
+  } else {
+    
+    if (verbose) cat(format(Sys.time(), "%x %X %Z"), ": Converting WTC features from list to dataframe\n")
+    
+    # initialise dataframe
+    df.out = data.frame()
+    
+    for (i in 1:length(ls)) {
+      # get Dyad and Time
+      Time = as.POSIXct(gsub(".*_", "", names(ls)[i]))
+      Dyad = gsub("_.*", "", names(ls)[i])
+      # convert wavelet coherence list into dataframe
+      df.out = rbind(
+        df.out,
+        data.frame(ls[[i]][["rsq"]]) |>
+          mutate(Period = ls[[i]][["period"]]) |>
+          tidyr::pivot_longer(cols = starts_with("X"), values_to = "Rsq") |>
+          merge(data.frame(ls[[i]][["phase"]]) |>
+                  mutate(Period = ls[[i]][["period"]]) |>
+                  tidyr::pivot_longer(cols = starts_with("X"), values_to = "Phase")) |>
+          merge(data.frame(ls[[i]][["signif"]]) |>
+                  mutate(Period = ls[[i]][["period"]]) |>
+                  tidyr::pivot_longer(cols = starts_with("X"), values_to = "PermProb")) |>
+          arrange(name) |>
+          mutate(
+            Timecourse = rep(ls[[i]][["t"]], each = length(ls[[i]][["period"]])),
+            COI   = rep(ls[[i]][["coi"]], each = length(ls[[i]][["period"]])),
+            Frame = as.numeric(gsub("X", "", name)),
+            Frequency = 1 / Period,
+            WithinCOI = Period < COI
+          ) |> select(-name) |>
+          mutate(
+            Dyad = Dyad,
+            Time = Time,
+            pseudoDyad = grepl("|", Dyad, fixed = T)
+          )
+      )
+    }
+    
+    
+    # save the data
+    if (!is.null(rs.path)) saveRDS(df.out, file = flnm)
+    
+  }
+  
+  if (return) return(df.out)
+  
+}
+
+#' Aggregate a dataframe containing the results of WTC to extract relevant features `[!MISSING]`
+#'
+#' Takes the dataframe created by [convertWTC()] and aggregates the results.
+#'
+#' @param df Dataframe. Dataframe created by [convertWTC()].
+#' @param rs.path Character. Path to destination directory for saved files. If empty (is.null(rs.path) == TRUE), then nothing is saved.
+#' @param featname Character. The name of the feature of which WTC was computed.
+#' @param suffix Character. Suffix to be added to the files saved to disk. Default is `""`.
+#' @param verbose Logical. Whether progress and output are printed to the console. Default is `TRUE`.
+#' @param recompute Logical. Whether existing data on disk should be recomputed and overwritten. Default is `FALSE`.
+#' @param return Logical. Whether the processed dataframe should be returned by the function. Default is `TRUE`.
+#'
+#' @return If `return = TRUE`, returns dataframe. If provided, the dataframe is saved as rds to `rs.path`.
+#' 
+#' @author Irene Sophia Plank (\email{10planki@@gmail.com})
+#' @import dplyr
+#' @export
+#' 
+
+featWTC = function(df, rs.path, featname, suffix = "", 
+                  verbose = T, recompute = F, return = T) {
+  
+
+  return(df)
+  
+}
