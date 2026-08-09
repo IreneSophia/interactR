25bcfd56f5d570bcbe87ab6d83ea89a103f86c28
IreneSophia <10planki@gmail.com>
2026-08-09 16:04:47 +0930

    new input catEFE allows for any categorisation of any emotional facial expression with associated columns

--- R/featEFE.R
+++ R/featEFE.R
@@ -2,13 +2,11 @@
 #' Combine ARKit52 Facial Expressions into Emotional Facial Expressions (EFE)
 #'
 #' This function combines individual facial expressions into emotional facial expressions
-#' (EFE) based on the classification proposed in Aldenhoven et al. (2026).
-#' While these are based on the ARKit52 system collected in the VERSE environment, 
-#' similar facial expressions can also be extracted using computer vision (e.g., OpenFace),
-#' provided the column names are adjusted to fit the system described below.
+#' (EFE) either based on the classification proposed in Aldenhoven et al. (2026) or
+#' by using a provided list of EFEs and associated columns. 
 #' 
 #' #' @details 
-#' Emotions are aggregates as the mean of the following Facial Expressions:
+#' If `catEFE` is set to Aldenhoven, then emotions are aggregates as the mean of the following Facial Expressions:
 #' 1. **Anger:** `"Facial_BrowDown.*"`, `"Facial_.*Eye_Squint"`, `"Facial_.*Eye_Wide"`, 
 #'   `"Facial_MouthPucker"`
 #' 2. **Disgust:** `"Facial_BrowDown.*"`, `"Facial_MouthFrown.*"`, `"Facial_MouthLowerDown.*"`, 
@@ -19,18 +17,15 @@
 #' 5. **Sadness:** `"Facial_BrowDown.*"`, `"Facial_BrowInnerUp"`, `"Facial_MouthFrown.*"`
 #' 6. **Surprise:** `"Facial_BrowInnerUp"`, `"Facial_BrowOuterUp.*"`, `"Facial_.*Eye_Wide"`, 
 #'   `"Facial_JawOpen"`
-#' 7. **Contempt:** Difference between sides in `"Facial_MouthDimple.*"`, `"Facial_MouthSmile.*"`
 #'
 #' @param df Dataframe containing the facial data. Must contain the columns `Dyad`, 
-#'   `Identifier`, `Time` and `Frame`, as well as columns with ARKit52 values. 
-#'   Specifically, the algorithm uses the following patterns (`.*` denotes sides):
-#'   `"Facial_BrowDown.*"`, `"Facial_.*Eye_Squint"`, `"Facial_.*Eye_Wide"`, 
-#'   `"Facial_MouthPucker"`, `"Facial_MouthFrown.*"`, `"Facial_MouthLowerDown.*"`, 
-#'   `"Facial_NoseSneer.*"`, `"Facial_CheekSquint.*"`, `"Facial_MouthSmile.*"`, 
-#'   `"Facial_BrowInnerUp"`, `"Facial_BrowOuterUp.*"`, `"Facial_JawOpen"`, 
-#'   `"Facial_MouthStretch.*"` and `"Facial_MouthDimple.*"`.
+#'   `Identifier`, `Time` and `Frame`, as well as columns detailed in `catEFE`.
 #'   If the dataframe contains a `Communication` column, EFEs are also
 #'   aggregated based on Speaking, Listening, Both and None.
+#' @param catEFE List or character. If it is character, then it must be "Aldenhoven" to use
+#'   the predefined categorisation of EFEs. If it is a list, then each list entry
+#'   should have the name of the EFE and contain a character vector with the associated column names. 
+#' @param rescaleVERSE Logical. Switch whether to correct for composite scores in VERSE Blendshapes. Default is `TRUE`.
 #' @param rs.path Character. Path to the directory where the output files will be saved.
 #'   If empty (`is.null(rs.path) == TRUE`), nothing is saved to disk. Default is `c()`.
 #' @param suffix Character. Suffix to be added to the files saved to disk. Default is `""`.
@@ -46,8 +41,9 @@
 #' @references Aldenhoven et al. (2026). Sensors.
 #' @export
 
-featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
-                    recompute = F, return = T) {
+featEFE = function(df, catEFE = "Aldenhoven2026", rescaleVERSE = T, 
+                   rs.path = c(), suffix = "", verbose = T,
+                   recompute = F, return = T) {
   
   # check rs.path
   if (is.null(rs.path)) {
@@ -69,61 +65,60 @@ featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
     } else {
       if (verbose) cat(format(Sys.time(), "%x %X %Z"), ": Preprocessing facial expressions\n")
       
-      # check the columns
+      # check the general columns
       checkDF(df, c("Dyad", "Identifier", "Frame", "Time"))
       
-      # check the facial columns
-      if (length(colnames(df |> 
-                          select(matches(
-                            c("Facial_BrowDown.*", "Facial_.*Eye_Squint", "Facial_.*Eye_Wide", 
-                              "Facial_MouthPucker", "Facial_MouthFrown.*", "Facial_MouthLowerDown.*", 
-                              "Facial_NoseSneer.*", "Facial_CheekSquint.*", "Facial_MouthSmile.*", 
-                              "Facial_BrowInnerUp", "Facial_BrowOuterUp.*", "Facial_JawOpen", 
-                              "Facial_MouthStretch.*", "Facial_MouthDimple.*"))))) < 25) {
-        warning("Dataframe df is containing less columns for extracting the emotions than expected.\n",
-                "If you aggregated across both sides of the face, this can be expected and is potentially no need to worry.\n",
-                "To be sure, please check the documentation of the function to assess whether you have\n",
-                "enough information to meaningfully interpret each emotion.")
+      # check if specified catEFE or Aldenhoven
+      if (!is.list(catEFE)) {
+        if (catEFE == "Aldenhoven2026") {
+          # use the Aldenhoven et al. (2026, Sensors) categorisation
+          catEFE = list(
+            "Anger" = c("Facial_BrowDownRight", "Facial_BrowDownLeft", "Facial_RightEye_Squint",
+                        "Facial_LeftEye_Squint", "Facial_LeftEye_Wide", "Facial_RightEye_Wide",
+                        "Facial_MouthPucker"),
+            "Disgust" = c("Facial_BrowDownRight",       "Facial_BrowDownLeft",        "Facial_MouthFrownLeft",      "Facial_MouthFrownRight",    
+                          "Facial_MouthLowerDownLeft",  "Facial_MouthLowerDownRight", "Facial_NoseSneerRight",      "Facial_NoseSneerLeft" ),
+            "Joy" = c("Facial_CheekSquintLeft",  "Facial_CheekSquintRight", "Facial_MouthSmileRight",  "Facial_MouthSmileLeft"),
+            "Fear" = c("Facial_BrowDownRight",   "Facial_BrowDownLeft",    "Facial_BrowInnerUp",     "Facial_BrowOuterUpLeft",
+                       "Facial_BrowOuterUpRight","Facial_RightEye_Squint", "Facial_LeftEye_Squint",  "Facial_LeftEye_Wide",   
+                       "Facial_RightEye_Wide",   "Facial_JawOpen",         "Facial_MouthStretchLeft","Facial_MouthStretchRight"),
+            "Sadness" = c("Facial_BrowDownRight", "Facial_BrowDownLeft",  "Facial_BrowInnerUp",   "Facial_MouthFrownLeft","Facial_MouthFrownRight"),
+            "Surprise" = c("Facial_BrowInnerUp",    "Facial_BrowOuterUpLeft","Facial_BrowOuterUpRight", "Facial_LeftEye_Wide",  
+                           "Facial_RightEye_Wide",  "Facial_JawOpen")
+          )
+        }
       }
+      
+      # check whether all columns named in catEFE are in the dataframe
+      if (length(setdiff(unlist(catEFE), names(df))) > 0) stop("The following columns do not exist in `df`: ", 
+                                                               paste(setdiff(unlist(catEFE), names(df)), collapse = ", "))
       
       # rescale some of the facial expressions from VERSE if they are composites
       # of multiple shapes
-      df = df |>
-        mutate(
-          Facial_BrowInnerUp      = Facial_BrowInnerUp / 2, # combines InnerBrowRaiserR and InnerBrowRaiserL
-          Facial_BrowOuterUpLeft  = Facial_BrowOuterUpLeft / 1.5, # takes 0.015 instead of 0.010
-          Facial_BrowOuterUpRight = Facial_BrowOuterUpRight / 1.5, # takes 0.015 instead of 0.010
-          Facial_CheekPuff        = Facial_CheekPuff / 2, # combines CheekPuffL and CheekPuffR
-          Facial_CheekSquintLeft  = Facial_CheekSquintLeft / 2, # combines CheekRaiserL and LipCornerPullerL
-          Facial_CheekSquintRight = Facial_CheekSquintRight / 2, 
-          Facial_LeftEye_Squint   = Facial_LeftEye_Squint / 1.25, # LidTightenerL and 0.0025 of LipCornerPullerL
-          Facial_MouthFunnel      = Facial_MouthFunnel / 1.5, # combines 0.0025 of 6 Blendshapes
-          Facial_MouthPucker      = Facial_MouthPucker / 1.3, # combines LipPuckerL and R with each 0.0065
-          Facial_RightEye_Squint  = Facial_RightEye_Squint / 1.25 # same as Left
-        )
+      if (rescaleVERSE) {
+        df = df |>
+          mutate(
+            Facial_BrowInnerUp      = Facial_BrowInnerUp / 2, # combines InnerBrowRaiserR and InnerBrowRaiserL
+            Facial_BrowOuterUpLeft  = Facial_BrowOuterUpLeft / 1.5, # takes 0.015 instead of 0.010
+            Facial_BrowOuterUpRight = Facial_BrowOuterUpRight / 1.5, # takes 0.015 instead of 0.010
+            Facial_CheekPuff        = Facial_CheekPuff / 2, # combines CheekPuffL and CheekPuffR
+            Facial_CheekSquintLeft  = Facial_CheekSquintLeft / 2, # combines CheekRaiserL and LipCornerPullerL
+            Facial_CheekSquintRight = Facial_CheekSquintRight / 2, 
+            Facial_LeftEye_Squint   = Facial_LeftEye_Squint / 1.25, # LidTightenerL and 0.0025 of LipCornerPullerL
+            Facial_MouthFunnel      = Facial_MouthFunnel / 1.5, # combines 0.0025 of 6 Blendshapes
+            Facial_MouthPucker      = Facial_MouthPucker / 1.3, # combines LipPuckerL and R with each 0.0065
+            Facial_RightEye_Squint  = Facial_RightEye_Squint / 1.25 # same as Left
+          )
+      }
       
       # preprocessing facial expressions
       df.face = df |> 
-        select(Dyad, Identifier, Time, Frame, any_of(c('Time', 'Timestamp', 'Partner', 'Actor', 'Communication')),
-               starts_with("Facial_")) |>
-        # code as emotions following Aldenhoven et al. (2026, Sensors)
-        mutate(
-          Anger = rowMeans(across(contains(c("BrowDown", "Eye_Squint", 
-                                             "Eye_Wide", "MouthPucker")))),
-          Disgust = rowMeans(across(contains(c("BrowDown", "MouthFrown", 
-                                               "MouthLowerDown", "NoseSneer")))),
-          Joy  = rowMeans(across(contains(c("CheekSquint", "MouthSmile")))),
-          Fear = rowMeans(across(contains(c("BrowDown", "BrowInnerUp", "BrowOuterUp", 
-                                            "Eye_Squint", "Eye_Wide", 
-                                            "JawOpen", "MouthStretch")))),
-          Sadness = rowMeans(across(contains(c("BrowDown", "BrowInnerUp", "MouthFrown")))),
-          Surprise = rowMeans(across(contains(c("BrowInnerUp", "BrowOuterUp", 
-                                                "Eye_Wide", "JawOpen")))),
-          # for contempt, Aldenhoven state only one side is active
-          # thus, we use the difference score between both sides
-          Contempt = abs(rowMeans(across(contains(c("MouthDimpleLeft", "MouthSmileLeft")))) - 
-                           rowMeans(across(contains(c("MouthDimpleRight", "MouthSmileRight")))))
-        )
+        (\(.data) {
+          colsEFE = purrr::imap(catEFE, ~ rowMeans(.data[.x], na.rm = TRUE))
+          names(colsEFE) = paste0("EFE_", names(colsEFE))
+          # 3. Bind the prefixed new columns to the dataframe
+          bind_cols(.data, as_tibble(colsEFE))
+        })()
       # get a list of columns that don't contain any data
       ls.cols = df.face |> 
         summarise(across(where(is.numeric), sum),
@@ -138,8 +133,8 @@ featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
     }
     # convert to long
     df.face = df.face |> select(-starts_with("Facial_")) |> 
-      tidyr::pivot_longer(cols = c(Anger, Disgust, Joy, Fear, Sadness, Surprise, Contempt), 
-                          names_to = "Emotion", names_prefix = "EFE.")
+      tidyr::pivot_longer(cols = starts_with("EFE_"), 
+                          names_to = "Emotion")
     # aggregate the emotional expressions
     df.out = merge(
       # individually for the emotions
@@ -148,7 +143,7 @@ featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
         summarise(value = mean(value),
                   .groups = "drop") |> 
         tidyr::pivot_wider(names_from = Emotion, 
-                    names_glue = "EFE_{Emotion}_Total"),
+                           names_glue = "{Emotion}_Total"),
       # over all emotions
       df.face |> 
         group_by(Dyad, Identifier, Time, across(any_of('Partner'))) |> 
@@ -163,7 +158,7 @@ featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
           summarise(value = mean(value),
                     .groups = "drop") |> 
           tidyr::pivot_wider(names_from = c(Emotion, Communication), 
-                      names_glue = "EFE_{Emotion}_{Communication}")) |>
+                             names_glue = "EFE_{Emotion}_{Communication}")) |>
         merge(
           df.face |> 
             group_by(Dyad, Identifier, Time, across(any_of('Partner')), Communication) |> 
@@ -183,5 +178,5 @@ featEFE = function(df, rs.path = c(), suffix = "", verbose = T,
   
   # return aggregated dataframe
   if (return) return(df.out)
-
+  
 }
