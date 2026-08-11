d87c6adb9d23cfc7326bb6538d321a9d77dd20cb
IreneSophia <10planki@gmail.com>
2026-08-11 09:44:40 +0930

    Instead of input function, precode useful summary statistics

--- R/featWLCC.R
+++ R/featWLCC.R
@@ -1,11 +1,11 @@
-#' Aggregate a dataframe containing the results of WLCC to extract relevant features 
+#' Aggregate Dataframe of WLCC Results
 #'
-#' Takes the dataframe created by [extractIPS()] with the method WLCC and aggregates the results.
-#' Identifier values contain leading by this Identifier, while Dyad values contain the overall
+#' Aggregates values extracted by [extractIPS()] with the method WLCC using the
+#' standard deviation, mean, median and mean of window peaks. Identifier values
+#' contain leading by this Identifier, while Dyad values contain the overall
 #' WLCC across all Lags. 
 #'
 #' @param df Dataframe. Dataframe created by [extractIPS()] with method WLCC.
-#' @param FUN Function. Function used for the aggregation across Lags for each window.
 #' @param absolute Logical. Switch whether to convert all the values to their 
 #'   absolute before aggregation. Default is `FALSE`.
 #' @param rs.path Character. Path to destination directory for saved files. 
@@ -15,14 +15,14 @@
 #' @param recompute Logical. Whether existing data on disk should be recomputed and overwritten. Default is `FALSE`.
 #' @param return Logical. Whether the processed dataframe should be returned by the function. Default is `TRUE`.
 #'
-#' @return If `return = TRUE`, returns dataframe. If provided, the dataframe is saved as rds to `rs.path`.
+#' @return If `return = TRUE`, returns dataframe. If provided, the dataframe is saved to `rs.path`.
 #' 
 #' @author Irene Sophia Plank (\email{10planki@@gmail.com})
 #' @import dplyr
 #' @export
 #' 
 
-featWLCC = function(df, FUN, absolute = F, r2z = F, rs.path = c(), suffix = "", 
+featWLCC = function(df, absolute = F, r2z = F, rs.path = c(), suffix = "", 
                    verbose = T, recompute = F, return = T) {
   
   if (verbose) cat("-------------- Extracting IPS features based on WLCC  --------------\n")
@@ -58,52 +58,71 @@ featWLCC = function(df, FUN, absolute = F, r2z = F, rs.path = c(), suffix = "",
     }
     # potentially remove sign from all WLCC values
     if (absolute) df = df |> mutate(WLCC = abs(WLCC))
-  
-    # get leading by Identifier
-    df.indi = df |>
+    
+    # prepare the data frame with Identifier information
+    df = df |> 
+      tidyr::drop_na() |>
       mutate(
         Identifier = case_when(
           Leading == "Left" ~ gsub("-.*", "", Dyad),
-          Leading == "Right" ~ gsub(".*-", "", Dyad)
+          Leading == "Right" ~ gsub(".*-", "", Dyad),
+          T ~ "Simultaneous"
         )
-      ) |> tidyr::drop_na() |>
-      # first, aggregate using FUN for each window
+      )
+    
+    # get the peaks for the Identifier leading and for the Dyad
+    df.peak = df |>
+      # first, aggregate using peak detection for each window
       group_by(Dyad, Time, Method, Identifier, Feature, Window) |>
-      filter(!is.na(WLCC)) |>
       summarise(
-        WLCC = FUN(WLCC),
-        .groups = "drop"
-      ) |> 
-      # then, aggregate for each Dyad over all windows
-      group_by(Dyad, Time, Method, Identifier, Feature) |>
-      summarise(
-        STD = sd(WLCC, na.rm = T),
-        AVG = mean(WLCC, na.rm = T),
+        indi = max(WLCC, na.rm = T),
         .groups = "drop"
       ) |>
-      tidyr::pivot_wider(names_from = Feature, values_from = c(AVG, STD),
+      group_by(Dyad, Time, Method, Feature, Window) |>
+      mutate(
+        dyad = max(indi, na.rm = T)
+      ) |> filter(Identifier != "Simultaneous") |>
+      # then, aggregate for over all windows
+      group_by(Dyad, Time, Method, Identifier, Feature) |>
+      summarise(
+        DyadWLCC = mean(indi, na.rm = T),
+        WLCC = mean(dyad, na.rm = T),
+        .groups = "drop"
+      ) |>
+      tidyr::pivot_wider(values_from = ends_with("WLCC"), 
+                         names_from = Feature, 
+                         names_glue = "{.value}_{Feature}_PEAK")
+  
+    # get leading by Identifier
+    df.indi = df |> filter(Identifier != "Simultaneous") |>
+      # aggregate for each Dyad and Identifier over all windows
+      group_by(Dyad, Time, Method, Identifier, Feature) |>
+      summarise(
+        STD  = sd(WLCC, na.rm = T),
+        AVG  = mean(WLCC, na.rm = T),
+        MED  = median(WLCC, na.rm = T),
+        .groups = "drop"
+      ) |>
+      tidyr::pivot_wider(names_from = Feature, 
+                         values_from = c(AVG, MED, STD),
                          names_glue = "WLCC_{Feature}_{.value}")
     
     # get overall WLCC
     df.dyad = df |>
-      # first, aggregate using FUN for each window
-      group_by(Dyad, Time, Method, Feature, Window) |>
-      filter(!is.na(WLCC)) |>
-      summarise(
-        WLCC = FUN(WLCC),
-        .groups = "drop"
-      ) |> 
-      # then, aggregate for each Dyad over all windows
+      # aggregate for each Dyad over all windows
       group_by(Dyad, Time, Method, Feature) |>
       summarise(
-        STD = sd(WLCC, na.rm = T),
-        AVG = mean(WLCC, na.rm = T),
+        STD  = sd(WLCC, na.rm = T),
+        AVG  = mean(WLCC, na.rm = T),
+        MED  = median(WLCC, na.rm = T),
         .groups = "drop"
       ) |>
-      tidyr::pivot_wider(names_from = Feature, values_from = c(AVG, STD),
+      tidyr::pivot_wider(names_from = Feature, 
+                         values_from = c(AVG, MED, STD),
                          names_glue = "DyadWLCC_{Feature}_{.value}")
     
     df.out = merge(df.indi, df.dyad) |> 
+      merge(df.peak) |>
       relocate(Dyad, Time, Identifier)
     
     # save the data
